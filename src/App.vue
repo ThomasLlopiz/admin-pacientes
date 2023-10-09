b515de4c81f8b80fb93766b4e01547bd1a38c546
ThomasLlopiz <toomillopiz@gmai.com>
2023-10-09 08:34:50 -0300

    limpiar formulario por object.assing

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,58 @@
 <script setup>
-import { ref, reactive } from 'vue';
-import { uid } from 'uid'
-import HeaderVue from './components/Header.vue';
-import Formulario from './components/Formulario.vue'
-import Paciente from './components/Paciente.vue'
+import { ref, reactive } from "vue";
+import { uid } from "uid";
+import HeaderVue from "./components/Header.vue";
+import Formulario from "./components/Formulario.vue";
+import Paciente from "./components/Paciente.vue";
 
-const pacientes = ref([])
+const pacientes = ref([]);
 
 const paciente = reactive({
   id: null,
-  nombre: '',
-  propietario: '',
-  email: '',
-  alta: '',
-  sintomas: '',
+  nombre: "",
+  propietario: "",
+  email: "",
+  alta: "",
+  sintomas: "",
 });
 
-const guardarPaciente = () =>{
+const guardarPaciente = () => {
   pacientes.value.push({
     ...paciente,
-    id: uid()
-  })
-  paciente.nombre= '',
-  paciente.propietario= '',
-  paciente.email= '',
-  paciente.alta= '',
-  paciente.sintomas= ''
-}
+    id: uid(),
+  });
+  Object.assign(alerta, {
+    nombre: "",
+    propietario: "",
+    email: "",
+    alta: "",
+    sintomas: "",
+  });
+};
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
-    <HeaderVue/>
+    <HeaderVue />
     <div class="mt-12 md:flex">
       <Formulario
-      v-model:nombre="paciente.nombre"
-      v-model:propietario="paciente.propietario"
-      v-model:email="paciente.email"
-      v-model:alta="paciente.alta"
-      v-model:sintomas="paciente.sintomas"
-      @guardar-paciente="guardarPaciente"
+        v-model:nombre="paciente.nombre"
+        v-model:propietario="paciente.propietario"
+        v-model:email="paciente.email"
+        v-model:alta="paciente.alta"
+        v-model:sintomas="paciente.sintomas"
+        @guardar-paciente="guardarPaciente"
       />
       <div class="md:w-1/2 md:h-screen">
-        <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
+        <h3 class="font-black text-3xl text-center">
+          Administra tus pacientes
+        </h3>
         <div v-if="pacientes.length">
           <p class="text-lg mt-5 text-center mb-10">
-            Información de 
+            Información de
             <span class="text-indigo-600 font-bold">Pacientes </span>
           </p>
-          <Paciente 
-            v-for="paciente in pacientes"
-            :paciente="paciente"
-          />
+          <Paciente v-for="paciente in pacientes" :paciente="paciente" />
         </div>
         <p v-else class="mt-10 text-2xl text-center">No hay pacientes</p>
       </div>

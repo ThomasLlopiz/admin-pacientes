8d29bc330409df419b87359b2277ea514a7cbf2e
ThomasLlopiz <toomillopiz@gmai.com>
2023-10-09 08:30:54 -0300

    correcion class bg-red

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { computed } from "vue";
 const props = defineProps({
   alerta: {
     type: Object,
@@ -7,14 +7,14 @@ const props = defineProps({
   },
 });
 const isError = computed(() => {
-    return props.alerta.tipo === 'error'
-})
+  return props.alerta.tipo === 'error'
+});
 </script>
 
 <template>
   <div
-  :class="[isError ? 'bg-red-500' : 'bg-green-500']"
-    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md bg-red-500"
+    :class="[isError ? 'bg-red-500' : 'bg-green-500']"
+    class="text-white text-center p-3 uppercase font-bold mb-3 rounded-md"
   >
     {{ alerta.mensaje }}
   </div>
